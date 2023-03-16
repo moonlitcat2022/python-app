5d334a52c816fc02f00d325e78beaab2a05aecce
moonlitcat2022 <127900684+moonlitcat2022@users.noreply.github.com>
2023-03-16 13:19:14 -0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -10,4 +10,4 @@ def timer():
 timer
 # what else should i do and we should have som way to show what the plans for this are so i know what you want me to do -hudson
 #Hey, if I need you to do somthing, I will make an issue (the button right next to code) and assign it to you. 
-#You can see that it has been assigned to you as your profile picture is next to it. Thanks. -Zach
+#You can see that it has been assigned to you as your profile picture is next to it. Also, I invited Alex and (not asshole) Caleb to this repo. Thanks. -Zach
