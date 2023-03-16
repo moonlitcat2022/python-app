4639a69019a37dec442b924ea54b8167aeb496f3
oriaura1 <115580698+oriaura1@users.noreply.github.com>
2023-03-16 09:59:20 -0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -3,3 +3,5 @@ def time(): #attemting to set time as a function
   1000ms=1second
 time.sleep(1second)#should wait for 1 second
 Print("hi again")
+
+#working on finding a way to have the delay work
