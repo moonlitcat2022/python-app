462bfdbc256b752456c34b06ea536ba8d33c7879
moonlitcat2022 <127900684+moonlitcat2022@users.noreply.github.com>
2023-03-16 13:18:10 -0700

    Adds message about issues

--- python/index.py
+++ python/index.py
@@ -8,4 +8,6 @@ def timer():
       if MsSenceStart==1000:
         break
 timer
-# what else should i do and we should have som way to show what the plans for this are so i know what you want me to do
+# what else should i do and we should have som way to show what the plans for this are so i know what you want me to do -hudson
+#Hey, if I need you to do somthing, I will make an issue (the button right next to code) and assign it to you. 
+#You can see that it has been assigned to you as your profile picture is next to it. Thanks. -Zach
