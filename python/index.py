77216b8996e9e4779fb03616f8cebea58aa3343f
moonlitcat2022 <127900684+moonlitcat2022@users.noreply.github.com>
2023-03-16 11:31:42 -0700

    Update index.py
    Fixes spelling and (possibly) formatting errors

--- python/index.py
+++ python/index.py
@@ -3,7 +3,7 @@ MsSenceStart=0
 
 def timer():
     time.sleep(.001)
-    MsSenceStart=MsSenceStart+1# tehr is an error with this and line 2 idk what it is
+    MsSenceStart = MsSenceStart + 1 #There is an error with this and line 2 and I dont know what it is
     print(MsSenceStart)
       if MsSenceStart==1000:
         break
