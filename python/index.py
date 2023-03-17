4e2eda3ab9581d572668aaf1ddb53a1a98e9399c
not-available1261 <moonlitcat2022@gmail.com>
2023-03-17 12:43:02 -0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -1,13 +1,10 @@
 import time
-MsSenceStart=0
+MsSenceStart = 0
 
 def timer():
     time.sleep(.001)
     MsSenceStart = MsSenceStart + 1 #There is an error with this and line 2 and I dont know what it is
     print(MsSenceStart)
-      if MsSenceStart==1000:
+      if MsSenceStart == 1000:
         break
 timer
-# what else should i do and we should have som way to show what the plans for this are so i know what you want me to do -hudson
-#Hey, if I need you to do somthing, I will make an issue (the button right next to code) and assign it to you. 
-#You can see that it has been assigned to you as your profile picture is next to it. Also, I invited Alex and (not asshole) Caleb to this repo. Thanks. -Zach
