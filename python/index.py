937adf914d77412208d153863955ca1f14bbf9c6
oriaura1 <115580698+oriaura1@users.noreply.github.com>
2023-03-21 08:38:21 -0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -1,12 +1,9 @@
 import time
-MsSenceStart = 0
 
-def timer():
-    time.sleep(.001)
-    MsSenceStart = MsSenceStart + 1 #There is an error with this and line 2 and I dont know what it is
-    print(MsSenceStart)
-      if MsSenceStart == 1000:
-        break
-timer
-
-
+def timer_start(length_of_timer):
+    seconds_sence_start = 0
+    while length_of_timer > seconds_sence_start:
+        time.sleep(1)
+        seconds_sence_start = seconds_sence_start + 1
+        print(seconds_sence_start)
+timer_start(int(input()))
